12eed6e5bb026b0451ae2b111e6a5a68ee65025a
Willyan <willyanalvesgomes@hotmail.com>
2026-03-08 15:56:33 -0300

    Criando métodos que modificam valores

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,11 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1978);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibirFichaTecnica();
         meuFilme.avalia(8);
---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,46 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacao;
+    private int duracaoEmMinutos;
+
+    public int getTotalDeAvaliacao() {
+        return totalDeAvaliacao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibirFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacao++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
+
+}

cff47d58945821e8ba55077e6f26d05112be5b24
Willyan <willyanalvesgomes@hotmail.com>
2026-03-08 13:05:05 -0300

    atribuindo métodos

--- src/Filme.java
+++ src/Filme.java
@@ -2,8 +2,23 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacao;
     int duracaoEmMinutos;
+
+    void exibirFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacao++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,16 @@
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
+        meuFilme.nome = "O Poderoso Chefão";
         meuFilme.anoDeLancamento = 1970;
         meuFilme.duracaoEmMinutos = 180;
 
-        System.out.println(meuFilme.nome);
+        meuFilme.exibirFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(7);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println(meuFilme.pegaMedia());
     }
 }
